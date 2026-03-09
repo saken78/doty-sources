c592b41ba2d835949af66f8b545236c60945ba76
saken78 <sakenspot@gmail.com>
2026-03-09 14:05:42 +0700

    config: update default configurations - bar, dock, hyprland, lockscreen, notch - overview, performance, prefix, system - theme, weather, workspaces

--- config/defaults/bar.js
+++ config/defaults/bar.js
@@ -20,12 +20,5 @@ var data = {
     "use12hFormat": false,
     "containBar": false,
     "keepBarShadow": false,
-    "keepBarBorder": false,
-    "itemsLeft": ["launcher", "workspaces", "layout", "pin"],
-    "itemsCenter": [],
-    "centerItemsSplitByNotch": false,
-    "itemsRight": ["presets", "tools", "systray", "controls", "battery", "clock", "power"],
-    "itemsLeftVertical": ["launcher", "systray", "tools", "presets"],
-    "itemsCenterVertical": ["layout", "workspaces", "pin"],
-    "itemsRightVertical": ["controls", "battery", "clock", "power"]
+    "keepBarBorder": false
 }
